21845293e06c85718e3c3fdd037057cb72544a0e
Kathy White <kawhite8503@gmail.com>
2022-08-09 21:42:29 -0500

    add detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('chores/', views.chores_index, name='chores_index'),
+  path('chores/<int:chore_id>/', views.chores_detail, name='chores_detail'),
   path('chores/create', views.ChoreCreate.as_view(), name='chores_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from .models import Chore
+from .models import Chore, Household
 
 def home(request):
   return render(request, 'home.html')
@@ -10,7 +10,22 @@ def about(request):
 
 def chores_index(request):
   chores = Chore.objects.all()
-  return render(request, 'chores/index.html', { 'chores': chores})
+  names = []
+  new_list = []
+  for chore in chores:
+    names.append(chore.assigned_to)
+  
+  for name in names:
+    if name not in new_list:
+      new_list.append(name)
+      print(new_list)
+
+  return render( request, 'chores/index.html', { 'chores': chores, 'names': names, 'new_list': new_list, 'Household': Household})
+
+def chores_detail(request, chore_id):
+  chore = Chore.objects.get(id=chore_id)
+  return render(request, 'chores/detail.html', { 'chore': chore})
+
 
 class ChoreCreate(CreateView):
   model = Chore
