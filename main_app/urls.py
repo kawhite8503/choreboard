bf08e109a8566b87a2c550e0f927bfd89f613bf5
Kathy White <kawhite8503@gmail.com>
2022-08-10 19:37:51 -0500

    remove slash

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('/', views.Home.as_view(), name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('chores/', views.chores_index, name='chores_index'),
   path('chores/<int:chore_id>/', views.chores_detail, name='chores_detail'),
